0121311a937a2de53167a1d6d2d167ad858b807f
Artyom M. Mustafin <a.mustafin@norsi-trans.ru>
2024-05-15 23:09:06 +0300

    Update tests

--- tests/test_get_money.py
+++ tests/test_get_money.py
@@ -2,22 +2,25 @@ from fixtures.create_atm_with_exist_banknotes import atm
 
 
 def test_get_money_with_negative_amount(atm):
+    amount_get_money = -1
     try:
-        atm.get_money(amount=-1)
+        atm.get_money(amount=amount_get_money)
     except ValueError as ex:
         assert ex.args[0] == 'Количество переданных банкнот должно быть больше нуля'
 
 
 def test_get_money_with_null_amount(atm):
+    amount_get_money = 0
     try:
-        atm.get_money(amount=0)
+        atm.get_money(amount=amount_get_money)
     except ValueError as ex:
         assert ex.args[0] == 'Количество переданных банкнот должно быть больше нуля'
 
 
 def test_get_money_with_error_type(atm):
+    amount_get_money = 'aaa'
     try:
-        atm.get_money(amount='aaa')
+        atm.get_money(amount=amount_get_money)
     except TypeError as ex:
         assert ex.args[0] == 'Передан неверный тип данных'
 
@@ -25,14 +28,13 @@ def test_get_money_with_error_type(atm):
 def test_get_money(atm):
     money_before_operation = atm.total_money
     vault_before_operation = atm.vault
+    amount_get_money = 1000
 
-    result = atm.get_money(1000)
-
-    assert atm.total_money == money_before_operation - 1000
-
+    result = atm.get_money(amount_get_money)
     vault_before_operation[1000] -= 1
-    assert atm.vault == vault_before_operation
 
+    assert atm.total_money == money_before_operation - amount_get_money
+    assert atm.vault == vault_before_operation
     assert result == {1000: 1}
 
 
@@ -44,8 +46,6 @@ def test_get_compare_money(atm):
 
     result = atm.get_money(amount_get_money)
 
-    assert atm.total_money == money_before_operation - amount_get_money
-
     vault_before_operation[5000] -= 1
     vault_before_operation[2000] -= 1
     vault_before_operation[1000] -= 2
@@ -54,6 +54,7 @@ def test_get_compare_money(atm):
     vault_before_operation[50] -= 1
     vault_before_operation[10] -= 3
 
+    assert atm.total_money == money_before_operation - amount_get_money
     assert atm.vault == vault_before_operation
     assert result == {10: 3, 50: 1, 100: 2, 500: 1, 1000: 2, 2000: 1, 5000: 1}
 
@@ -61,14 +62,19 @@ def test_get_compare_money(atm):
 def test_get_money_more_total(atm):
     amount_get_money = 100_000
     result = atm.get_money(amount=amount_get_money)
+
     assert result == 'Денег недостаточно'
+
 
 def test_get_money_less_min_banknote(atm):
     amount_get_money = 1
     result = atm.get_money(amount=amount_get_money)
+
     assert result == 'Невозможно собрать необходимую сумму с существующими купюрами'
+
 
 def test_get_with_incomparable_banknotes(atm):
     amount_get_money = 190
     result = atm.get_money(amount=amount_get_money)
+
     assert result == 'Невозможно собрать необходимую сумму с существующими купюрами'
